4bcb6e16022c49aee43852be358163af779adeea
Tammer <tammer@tammer.com>
2023-08-27 11:51:56 -0400

    .

--- src/streambatch/savgol.py
+++ src/streambatch/savgol.py
@@ -23,20 +23,23 @@ def find_outliers(m2):
     m2['outlier'] = (m2['delta'].abs() > 0.15) & (m2['zscore'].abs() > 1.5)
     return m2
 
+# remove_outliers()
 # m is a dataframe with columns time and ndvi
+# returns a dataframe with outliers removed
 def remove_outliers(m):
     m1 = m.copy()
     m1 = find_outliers(m1)
     m2 = m1[m1['outlier']==False]
     # print(f"removed {m1.shape[0] - m2.shape[0]} outliers")
-    # print("here")
-    # print(m2.head())
-    # remove the last 5 columns of m2
     m2 = m2.drop(columns=['rolling mean', 'rolling std', 'zscore', 'delta', 'outlier'])
     return m2
 
+
+# handle_duplicates()
+# sometimes we end up with observations from s2 and l8 on the same day.
+# when this happens, we need reduce to one observation per day
+# !!! a better solution is the choose the "better" observation based on before and after
 def handle_duplicates(m):
-    # !!! a better solution is the choose the "better" observation based on before and after
     # find successive rows with the same time
     # if there are two rows with the same time, keep the first one
     m1 = m.copy()
@@ -46,6 +49,12 @@ def handle_duplicates(m):
     m2 = m2.drop(columns=['duplicate'])
     return m2
 
+# prepare()
+# m is a dataframe with columns time, ndvi.sentinel2, ndvi.landsat, qa.sentinel2, qa.landsat
+# (basically the output from the server if you ask for sentinel2 and landsat data)
+# We convert this into a sparse dataframe with columns time, ndvi
+# where there is only an entry if we have a valid ndvi value for that date from at least one source
+# we return (m, s2, l8), where m is the sparse dataframe, s2 is the original sentinel2 data, l8 is the original landsat data
 def prepare(df):
     # is this points or polygons?
     sort_col = 'point'
@@ -86,41 +95,28 @@ def prepare(df):
 
 def savgol_(m,s2,l8,window_length=20,polyorder=2):
     m = remove_outliers(m)
-    # print("After remove_outliers")
-    # print(m.head())
     min_date = m["time"].min()
-    # print(min_date)
     max_date = m["time"].max()
-    # print(max_date)
     date_range = pd.date_range(min_date, max_date, freq='D')
-    # print("date_range")
-    # print(date_range.shape)
 
+    # Create a new DataFrame 'm1' with a column 'time' that contains all dates in the range
     m1 = pd.DataFrame({"time": date_range})
 
-    # Merge the original 'ndvi' values into the new DataFrame 'm1' using outer join
+    # Populate it with NDVI values where we have them and NaN where we don't
     m1 = pd.merge(m1, m, on="time", how="left")
-    # print("m1")
-    # print(m1.shape)
-    # print(m1.head(2))
-    # print(m1.tail(2))
+    m1 = m1.fillna(method='ffill') # forward fill the other columns like lat, lon, point, location, etc
 
-    # replace any NAN with the value from the previous row
-    # print("before ffil")
-    # print(m1.tail(30))
-    m1 = m1.fillna(method='ffill')
-    # if there is a column called "point" coerce it to int
+    # if there is a column called "point" or "location" coerce it to int
     if "point" in m1.columns:
         m1["point"] = m1["point"].astype(int)
     if "location" in m1.columns:
         m1["location"] = m1["location"].astype(int)
-    # print("After merge")
-    # print(m1.tail(30))
 
     # Interpolate missing values in the 'ndvi' column
     m1["ndvi"] = m1["ndvi"].interpolate()
 
     # Fill missing values with NaN, so the filter doesn't treat them as zeros
+    # !!! there shouldnt be any missing values at this point. not sure I need this
     m1["ndvi"] = m1["ndvi"].replace(0, np.nan)
 
     # Apply the savgol_filter to smooth the "ndvi" column
@@ -129,31 +125,37 @@ def savgol_(m,s2,l8,window_length=20,polyorder=2):
     # drop column ndvi
     m1 = m1.drop(columns=['ndvi'])
 
+    # now let's add two more columns with the original sentinel2 and landsat values
+    # First S2
     temp = pd.DataFrame({"time": date_range})
-    # merge in the original ndvi.sentinel2 values
+    # Populate it with NDVI values where we have them and NaN where we don't
     temp = pd.merge(temp, s2, on="time", how="left")
-    # drop column polygon
-    temp = temp.drop(columns=['polygon'])
-    # print("temp1")
-    # print(temp.tail(20))
-    # print(temp.shape)
-    # print(m1.shape)
+    # add it as a column to m1
     m1['ndvi.sentinel2'] = temp['ndvi.sentinel2']
-    # merge in the original ndvi.landsat values
+
+    # and now Landsat
     temp = pd.DataFrame({"time": date_range})
     temp = pd.merge(temp, l8, on="time", how="left")
-    # print("temp l8")
-    # print(temp.tail(20))
     m1['ndvi.landsat'] = temp['ndvi.landsat']
+
     return m1
 
-
+# savgol()
+# df_ is a dataframe with columns time, ndvi.sentinel2, ndvi.landsat, qa.sentinel2, qa.landsat
+# (basically the output from the server if you ask for sentinel2 and landsat data)
+# Returns a dataframe with columns time, ndvi, ndvi.sentinel2, ndvi.landsat
+# where ndvi is the smoothed savgol ndvi
 def savgol(df_,window_length=20,polyorder=2):
+    # take the raw data that has columns for both sentinel2 and landsat
+    # and convert it into a sparse dataframe with columns time, ndvi (merging the two ndvi columns)
     (m,s2,l8) = prepare(df_)
+    
+    # is this points or polygons?
     sort_col = 'point'
     if m.columns[0] != 'point':
         sort_col = 'location'
 
+    # process each point or location separately
     for p in m[sort_col].unique():
         m1 = m[m[sort_col]==p]
         s21 = s2[s2[sort_col]==p]
